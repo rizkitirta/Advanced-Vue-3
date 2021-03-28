428f4aa9ed1655c73d57caf21f809a14932a4431
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-03-28 22:53:29 +0700

    Declrative Rendering | Vue 3

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div>
+    {{message}}
+  </div>
+  <span :title='message'>blalala</span>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World'
+    }
+  },
+}
+</script>
+<style lang="">
+  
+</style>
