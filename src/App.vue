2910f917a647ffbab7977c3147c853628ed97724
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-03-29 23:23:36 +0700

    v-once | Vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,19 @@
 <template lang="">
-<div id="lifecycle"> {{ message }} </div>
-</template>
+    <div v-once>
+        {{ message }}
+    </div>
 
+    <button @click="message = 'Hello world'">update</button>
+</template>
 <script>
-import childComponent from "./components/childComponent.vue";
 export default {
     data() {
         return {
-            message: 'Hello world'
+            message: 'Hi bro!'
         }
     },
-    beforeCreate() {
-        console.log('before crated');
-        console.log(this.message);
-        console.log(document.getELementById('lifecycle'));
-    },
-    created() {
-        console.log('created')
-    },
-    beforeMount() {
-        console.log('before mount')
-    },
-    mounted() {
-        console.log('mounted')
-    },
-    beforeUpdate() {
-        console.log('before update')
-    },
-    updated() {
-        console.log('updated')
-    },
-    onBeforeUnmount() {
-        console.log('before destroy')
-    },
-    unmounted() {
-        console.log('before destroyed')
-    },
-};
+}
 </script>
-
-<style lang=""></style>
+<style lang="">
+    
+</style>
