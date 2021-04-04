c637fcf31c39cb3ea6575c260ae8b550298dd4a7
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-04-04 22:39:26 +0700

    Watcher | Vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,22 @@
 <template lang="">
-<div> {{ discount }} </div>
+<div> {{ message }} </div>
 
-<input v-model="num1"/>
-<input v-model="num2"/>
+<input v-model="message"/>
 
 <button @click="addDiscount">+ discount</button>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            num1:0,
-            num2:0
-        }
-    },
-    computed:{
-        discount: {
-            get() {
-                return parseInt(this.num1) + parseInt(this.num2);
-            },
-            set(value) {
-                this.num1 -= value;
-                this.num2 -= value;
-            }
-        }
-    },
-     methods: {
-            addDiscount() {
-                this.discount = 500;
-            }
-        },
+  data() {
+      return {
+          message: 'Hello World'
+      }
+  },
+  watch: {
+      message(value) {
+          console.log('Perubahan di message' + value);
+      }
+  }
 }
 </script>
 <style lang="">
