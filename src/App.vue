527dcb24952fd8dd8f17f381c7789fedcbea140c
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-04-08 22:54:42 +0700

    Global Component | Vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,9 @@
 <template lang="">
-<child>
-    <template v-slot:default="{ data }">
-        <span>Judul : {{data}}</span>
-    </template>
-
-    <template v-slot:slot2="{data}">
-        <span>Konten : {{ data }}</span>
-    </template>
-
-    <template v-slot:slot3="{data}">
-        <span>Footer : {{data}}</span>
-    </template>
-</child>
+<child-component />
 </template>
 
 <script>
-import Child from "./components/childComponent.vue";
 export default {
-    components: {
-        Child
-    },
     data() {
         return {
             message: ""
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import ChildComponent from "./components/childComponent.vue";
+
+const app = createApp(App);
+app.component("child-component", ChildComponent);
+app.mount("#app");
